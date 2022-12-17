518abe334bb02e4cbfd843750da3b309ac9fc429
Franklin Okech <franklinokech@gmail.com>
2022-12-17 09:29:45 +0300

    added contact us form

--- frankresume/settings.py
+++ frankresume/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io']
+
 
 # Application definition
 
